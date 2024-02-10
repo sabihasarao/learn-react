8d6970d4ca8812173b0bcbbbc7ff84b18d80a32b
Sabiha Sarao <sarao.s@northeastern.edu>
2024-02-10 12:23:10 -0500

    020924

--- src/pages/qcomps/firstcomp.js
+++ src/pages/qcomps/firstcomp.js
@@ -1,1 +1,10 @@
-function MyComp() {}
+export default function MyComp() {}
+return (
+    <div>
+        <h1> My first component</h1>
+        <o1> 
+            <li>Componenets: UI Building Block</li>
+            <li>Defining a Component</li>
+            <li>Using a Component</li>
+    </div>
+)
